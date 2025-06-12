ada7a1aa61a7545fe48176ee0da6370fef2502d9
Rohan <rohan.kr.pandey2.0@gmail.com>
2025-06-12 22:42:09 +0530

    office-workspace-

--- src/pages/OfficeWorkSpace.jsx
+++ src/pages/OfficeWorkSpace.jsx
@@ -1,6 +1,4 @@
 import React, { useState, lazy, Suspense } from "react";
-
-
 import Banner from "../common/Banner";
 import NavBar from "../common/NavBar";
 import WhyChooseUs from "../components/productandservices/diyprefabkits/WhyChooseUs";
@@ -16,55 +14,39 @@ const WaveWrapper = lazy(() => import("../common/WaveWrapper"));
 const Footer = lazy(() => import("../common/Footer"));
 const VideoModal = lazy(() => import("../common/VideoModal"));
 
-
-
-
 const OfficeWorkSpace = () => {
+  const [openVideo, setOpenVideo] = useState(false);
+  const handleOpenVideo = () => setOpenVideo(!openVideo);
+  const seo = useSeoHelmet("office-workspaces");
 
-    const [openVideo, setOpenVideo] = useState(false);
-    const handleOpenVideo = () => setOpenVideo(!openVideo);
-    const seo = useSeoHelmet("office-workspaces"); // Fetch SEO by slug
+  return (
+    <div>
+      <SeoHelmet seo={seo} />
+      <NavBar />
+      <Banner
+        title={"Office & Workspaces"}
+        description={"Engineering Excellence, Industrial Solutions"}
+      />
+      <OurServices />
+      <Suspense fallback={<div>Loading trust slider...</div>}>
+        <TrustSlider />
+      </Suspense>
+      <WeOffer />
+      <Suspense fallback={<div>Loading counter...</div>}>
+        <CounterCard />
+      </Suspense>
+      <WhyChooseUs handleOpenVideo={handleOpenVideo} />
+      <Suspense fallback={<div>Loading visual section...</div>}>
+        <WaveWrapper />
+      </Suspense>
+      <Suspense fallback={<div>Loading footer...</div>}>
+        <Footer />
+      </Suspense>
+      <Suspense fallback={null}>
+        <VideoModal open={openVideo} onClose={handleOpenVideo} />
+      </Suspense>
+    </div>
+  );
+};
 
-
-
-
-
-    return (
-        <div>
-            <SeoHelmet seo={seo} />
-
-            <NavBar />
-            <Banner
-                title={"Office & Workspaces"}
-                description={"Engineering Excellence, Industrial Solutions"}
-            />
-
-            <OurServices />
-
-
-
-            <Suspense fallback={<div>Loading trust slider...</div>}>
-                <TrustSlider />
-            </Suspense>
-            <WeOffer />
-
-            <CounterCard />
-
-            <WhyChooseUs handleOpenVideo={handleOpenVideo} />
-
-            <Suspense fallback={<div>Loading visual section...</div>}>
-                <WaveWrapper />
-            </Suspense>
-
-            <Suspense fallback={<div>Loading footer...</div>}>
-                <Footer />
-            </Suspense>
-            {/* Video Modal */}
-            <Suspense fallback={null}>
-                <VideoModal open={openVideo} onClose={handleOpenVideo} />
-            </Suspense>
-        </div>
-    )
-}
-
-export default OfficeWorkSpace
+export default OfficeWorkSpace;
