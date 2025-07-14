500fe0c5991d22ccb9e492a953319a00f2d972da
Roh4n <rohan.pandey2.0@gmail.com>
2025-07-14 13:56:34 +0530

    css-issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./Responsive.css";
 import { Route, Routes } from "react-router-dom";
 import RoutesPath from './helper/Routes/Routes'
 import PageNotFound from "./pages/PageNotFound";
